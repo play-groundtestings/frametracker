c39348ad652b29f55f75d2182f7ec42cacc94454
play-groundtestings <115119352+play-groundtestings@users.noreply.github.com>
2025-12-01 17:26:14 +0800

    new code
    vue updated code

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,46 @@
 <script>
-
 export default {
   name: 'App',
+
   data() {
     return {
-      framelist: [],
-      myindex: 0,
-      inputjonumber: ""
+      framelist: [],       // full sheet data
+      myindex: -1,         // -1 means "not found"
+      inputjonumber: ""    // user input
     }
   },
-  computed: {
-    findResults() {
-      let index
-      console.log(this.framelist.values)
-      for (var i = 0; i < this.framelist.values.length; i++) {
-        for (var k = 0; k < this.framelist.values[0].length; k++) {
-          if (this.framelist.values[i][k] == this.inputjonumber) {
-            index = i
-            this.myindex = index
-          }
-        }
-      }
-    },
-  },
+
   methods: {
     async getData() {
-      const res = await fetch("https://sheets.googleapis.com/v4/spreadsheets/186-7MMQBauKDWO4E9Vk-4hbVhTVPYtMHRSjRK9ZITpQ/values/In-Studio%20Shoots/?alt=json&key=AIzaSyCkPCG3NLQ4bMpRQsDPir9mKFgKvybmQvE")
-      const finalRes = await res.json()
-      this.framelist = finalRes
+      const res = await fetch(
+        "https://sheets.googleapis.com/v4/spreadsheets/186-7MMQBauKDWO4E9Vk-4hbVhTVPYtMHRSjRK9ZITpQ/values/In-Studio%20Shoots/?alt=json&key=AIzaSyCkPCG3NLQ4bMpRQsDPir9mKFgKvybmQvE"
+      );
+      const finalRes = await res.json();
+      this.framelist = finalRes;
+      console.log("Loaded rows:", this.framelist.values?.length);
+    },
+
+    findResults() {
+      if (!this.framelist.values) return;
+
+      this.myindex = -1; // reset
+
+      // JO Number is column index 2
+      for (let i = 0; i < this.framelist.values.length; i++) {
+        const row = this.framelist.values[i];
+
+        if (row[2] && row[2].toString() === this.inputjonumber.toString()) {
+          this.myindex = i;
+          return;
+        }
+      }
     }
   },
-  components: {
-  },
+
   mounted() {
-
-    this.getData()
-
+    this.getData();
   }
 }
-
 </script>
 
 <template>
@@ -46,68 +48,66 @@ export default {
 
   <main>
     <div class="wrapper">
-      <div style="text-align: center;"> <B>FRAME AVAILABILITY</B> </div> <hr>
-      <div> <input v-model="inputjonumber" placeholder="Input your J.O. number"> <button id="findButton" @click="findResults">Check</button> </div>
+      <div style="text-align: center;"><b>FRAME AVAILABILITY</b></div>
+      <hr>
 
-      <div v-if="myindex === 0"> <b>J.O. Number:</b> {{ inputjonumber }} </div>
-      <div v-else>
-        <b>J.O. Number: </b> {{ inputjonumber }} <br>
-        <b>Name: </b> {{ framelist.values[this.myindex][0] }}<br>
-        <b>Ready for Pickup: </b>
-        
-        <span v-if="framelist.values[this.myindex][5]=='yes'" style="color:green">
-          ✔
-        </span>
-        <span v-else style="color:red">
-          ✘
-        </span>
+      <div>
+        <input v-model="inputjonumber" placeholder="Input your J.O. number">
+        <button id="findButton" @click="findResults">Check</button>
+      </div>
 
+      <!-- NOTHING FOUND -->
+      <div v-if="myindex === -1 && inputjonumber">
+        <b>J.O. Number:</b> {{ inputjonumber }} <br>
+        No matching record found.
+      </div>
 
+      <!-- FOUND -->
+      <div v-if="myindex >= 0">
+        <b>J.O. Number:</b> {{ inputjonumber }} <br>
+        <b>Name:</b> {{ framelist.values[myindex][0] }}<br>
+
+        <b>Ready for Pickup:</b>
+        <span v-if="framelist.values[myindex][5] === 'yes'" style="color:green">✔</span>
+        <span v-else style="color:red">✘</span>
       </div>
     </div>
-    <div v-if="myindex === 0"> </div>
-    <div v-else>
-      <div v-if="framelist.values[this.myindex][5]=='yes'">
-        <div class="wrapper">
 
-          Your photo package is ready for pick-up! Please text Dalyn at least one day before you plan to pick up your package or send over a courier (Grab/Lalamove). This would be highly appreciated! <br><br>
-          Pick-Up Details:<br><br>
+    <!-- READY FOR PICKUP MESSAGE -->
+    <div v-if="myindex >= 0">
+      <div v-if="framelist.values[myindex][5] === 'yes'">
+        <div class="wrapper">
+          Your photo package is ready for pick-up! Please text Dalyn at least one day before you plan to pick up your package or send over a courier (Grab/Lalamove).<br><br>
+
+          <b>Pick-Up Details:</b><br><br>
           Name: Dalyn <br>
           Contact Number: +63 917 577 3406 <br>
           Hours: 9AM-4PM <br>
-          Address: 2nd Floor, 105-E Mariano Ponce St., Caloocan City    <br>
-          <!-- <i style="color: #242320; font-size:smaller">*We have a pin on Grab. Just type “TinoLey Digital Photography, Inc.”</i><br><br> -->
-          Note: Please text Dalyn 1-day before you pick up your photo package so she can prepare it! <br>
+          Address: 2nd Floor, 105-E Mariano Ponce St., Caloocan City <br><br>
 
+          Note: Please text Dalyn 1-day before you pick up your photo package so she can prepare it!
         </div>
       </div>
+
+      <!-- NOT READY -->
       <div v-else>
         <div class="wrapper">
-
           Your frame is not ready yet. Please check again later.
-
         </div>
       </div>
     </div>
-
   </main>
 </template>
 
 <style scoped>
-
-.wrapper{
+.wrapper {
   border: 3px solid aliceblue;
   background-color: gainsboro;
-  padding-left: 2%;
-  padding-top: 2%;
-  padding-bottom: 2%; 
-  padding-right: 2%;
+  padding: 2%;
   width: 20%;
   font-family: Arial, Helvetica, sans-serif;
 }
-
-input{
-  margin-bottom:5%;
+input {
+  margin-bottom: 5%;
 }
-
 </style>
